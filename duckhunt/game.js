86eeda3ee34375bcf6b6782d26b3b7b805885077
Shriya K Nevatia <sneva831@gmail.com>
2014-02-27 23:48:17 -0500

    Drew a dog and got JS to work

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,11 +1,41 @@
-draw () {
-	// sky is #87CEEB
-	// road is #C96A1B
-	// bushes
-	// a tree
-	// dog sniffing the road
-	// 5 flying birds in the skies (bird color of our choice)
-	drawImage () {
-		// to slice image at particular coordinates
+
+function start() {
+	canvas = document.getElementById('game');
+	ctx = canvas.getContext('2d');
+
+	mypic = new Image();
+	mypic.src = 'assets/duckhunt.png';
+
+	draw();
+}
+
+function draw () {
+	mypic.onload = function () {
+		//ctx.drawImage(mypic, topleft x, topleft y, width, height, x on canvas, y on canvas, width on canvas, height on canvas); 
+		
+		//grass and road and bush
+		ctx.drawImage(mypic, 585, 58, 59, 44, 350, 420, 118, 88); 
+		console.log("stuff");
+		//dog
+		ctx.drawImage(mypic, 0, 0, 60, 50, 300, 500, 120, 100); 
+		
+		//tree
+		ctx.drawImage(mypic, 0, 0, 60, 50, 300, 500, 120, 100); 
+
+		//duck 1
+		//ctx.drawImage(mypic, topleft x, topleft y, width, height, x on canvas, y on canvas, width on canvas, height on canvas); 
+		
+		//duck 2
+		//ctx.drawImage(mypic, topleft x, topleft y, width, height, x on canvas, y on canvas, width on canvas, height on canvas); 
+	
+		//duck 3
+		//ctx.drawImage(mypic, topleft x, topleft y, width, height, x on canvas, y on canvas, width on canvas, height on canvas); 
+	
+		//duck 4
+		//ctx.drawImage(mypic, topleft x, topleft y, width, height, x on canvas, y on canvas, width on canvas, height on canvas); 
+	
+		//duck 5
+		//ctx.drawImage(mypic, topleft x, topleft y, width, height, x on canvas, y on canvas, width on canvas, height on canvas); 
+	
 	}
 }
