acf308f5ad6833e681f7f514b0560ac31c9bb4b9
Shriya K Nevatia <sneva831@gmail.com>
2014-02-27 23:59:30 -0500

    DONE

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -13,29 +13,29 @@ function draw () {
 	mypic.onload = function () {
 		//ctx.drawImage(mypic, topleft x, topleft y, width, height, x on canvas, y on canvas, width on canvas, height on canvas); 
 		
+		//tree
+		ctx.drawImage(mypic, 0, 270, 80, 130, 30, 250, 160, 260); 
+
 		//grass and road and bush
 		ctx.drawImage(mypic, 100, 710, 800, 190, 0, 410, 800, 190); 
 
 		//dog
 		ctx.drawImage(mypic, 0, 0, 60, 50, 300, 450, 180, 150); 
-		
-		//tree
-		ctx.drawImage(mypic, 0, , 60, 50, 300, 500, 120, 100); 
 
 		//duck 1
-		//ctx.drawImage(mypic, topleft x, topleft y, width, height, x on canvas, y on canvas, width on canvas, height on canvas); 
+		ctx.drawImage(mypic, 0, 115, 35, 35, 130, 100, 70, 70); 
+
+		// duck 2
+		ctx.drawImage(mypic, 40, 115, 35, 35, 200, 120, 70, 70)
+
+		// duck 3
+		ctx.drawImage(mypic, 80, 115, 35, 35, 300, 250, 70, 70); 	
 		
-		//duck 2
-		//ctx.drawImage(mypic, topleft x, topleft y, width, height, x on canvas, y on canvas, width on canvas, height on canvas); 
-	
-		//duck 3
-		//ctx.drawImage(mypic, topleft x, topleft y, width, height, x on canvas, y on canvas, width on canvas, height on canvas); 
-	
-		//duck 4
-		//ctx.drawImage(mypic, topleft x, topleft y, width, height, x on canvas, y on canvas, width on canvas, height on canvas); 
-	
-		//duck 5
-		//ctx.drawImage(mypic, topleft x, topleft y, width, height, x on canvas, y on canvas, width on canvas, height on canvas); 
+		// duck 4
+		ctx.drawImage(mypic, 80, 155, 40, 35, 480, 200, 70, 70); 	
+		
+		// duck 5
+		ctx.drawImage(mypic, 120, 155, 40, 35, 410, 80, 70, 70); 	
 	
 	}
 }
