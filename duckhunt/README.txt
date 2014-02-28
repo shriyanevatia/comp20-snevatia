02641d62a3fa1a493e5915bb36a6d802ca73644f
Shriya K Nevatia <sneva831@gmail.com>
2014-02-27 21:02:29 -0500

    Created readme and started adding JS functions

---
+++ duckhunt/README.txt
@@ -0,0 +1,15 @@
+README
+Shriya Nevatia
+Assignment 2 
+
+Used 2 Tokens; Submitted 2 days late
+
+- Correctly Implemented:
+
+- Incorrectly Implemented / Didn't get to:
+
+- Collaborated with:
+Nobody
+
+- Hours Spent: 
+1, 
--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,11 @@
-// Your work goes here...
+draw () {
+	// sky is #87CEEB
+	// road is #C96A1B
+	// bushes
+	// a tree
+	// dog sniffing the road
+	// 5 flying birds in the skies (bird color of our choice)
+	drawImage () {
+		// to slice image at particular coordinates
+	}
+}
